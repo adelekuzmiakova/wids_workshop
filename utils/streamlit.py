f3db43ad9fbc7a64a5159871698fafed9d13285b
adele.kuzmiakova <adele02@pm.me>
2023-03-08 19:21:27 +0100

    updated code

--- utils/streamlit.py
+++ utils/streamlit.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def load_expander_crop_hints():
-    expander = st.expander("**What is crop hints?**")
-    expander.write(" • **Crop hints** identifies the **area of interest** in your photo and removes distracting elements.   \n • You can specify the **aspect (width:height) ratio**, such as 1:1 (square) or 16:9 (long panorama), of the final cropped image using the left sidebar).  \n • Then upload your image and you will be shown the **cropped version**.  \n   \n**An example:**")
+    expander = st.expander("**What is dominant colors?**")
+    expander.write(" • **Dominant colors** identifies the colors that **are represented the most** in your photo.  \n • Use the slider below to select the number of dominant colors you want to pull out from your image    \n • The dominant colors with their proportions will be shown.  \n   \n**An example:**")
     expander.image("https://raw.githubusercontent.com/ifolor/ml-demo-app/dev/main/public_assets/cover_image_crop.png", width = 700) 
 
 def write(text, variable):
